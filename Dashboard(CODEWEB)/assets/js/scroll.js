a9432bbb6790ba1570049c99fa14c76dade3ff36
Samyr Uchoa Salem <samyruchoasalem@hotmail.com>
2023-05-19 16:19:34 -0400

    Consertando links, estilizando as scrollbar, etc...

---
+++ Dashboard(CODEWEB)/assets/js/scroll.js
@@ -0,0 +1,8 @@
+$("body").niceScroll({
+    cursorcolor: "#9C27B0",
+    cursorwidth: "5px",
+    cursorborder: "none",
+    cursorborderradius: "5px",
+    autohidemode: false,
+    railpadding: { top: 0, right: 3, left: 3, bottom: 0 }
+});
